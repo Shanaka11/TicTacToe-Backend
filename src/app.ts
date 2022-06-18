98326e0878de08611b7d4352452bef360a7a014e
shanaka11 <shanakaabeysinghe@gmail.com>
2022-06-18 17:32:46 +0530

    Refactored server creation logic

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
-import express from 'express'
 import config from 'config'
-
+import createServer from './utils/server'
 import connect from './utils/connect'
 import logger from './utils/logger'
-import routes from './routes'
 
 const port = config.get<number>('port')
-const app = express()
-app.use(express.json())
+
+const app = createServer()
 
 app.listen(port, async () => {
     logger.info(`App is runnig at http://localhost:${port}`)
     await connect()
-    routes(app)
 })
